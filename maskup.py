59bd003935d010f62a93097af0fd7f3d0eb67c58
emidoan <69323763+emidoan@users.noreply.github.com>
2020-09-19 18:52:40 -0700

    Add files via upload
    emily's commit

--- maskup.py
+++ maskup.py
@@ -15,6 +15,7 @@ VIEWPORT_MARGIN = 200
 MOVEMENT_SPEED = 10 * SPRITE_SCALING
 GRAVITY = 0.9 * SPRITE_SCALING
 JUMP_SPEED = 28 * SPRITE_SCALING
+BULLET_SPEED = 5 * SPRITE_SCALING
 
 class MyGame(arcade.Window):
     """ Main application class. """
@@ -35,6 +36,7 @@ class MyGame(arcade.Window):
         self.wall_list = None
         self.player_list = None
         self.enemy_list = None
+        self.bullet_list = None
 
         # Set up the player
         self.player_sprite = None
@@ -47,6 +49,7 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
 
         # Set up the player
         self.player_sprite = arcade.Sprite("sprite2.png",0.025)
@@ -112,6 +115,7 @@ class MyGame(arcade.Window):
         self.wall_list.draw()
         self.player_list.draw()
         self.enemy_list.draw()
+        self.bullet_list.draw()
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
@@ -132,24 +136,52 @@ class MyGame(arcade.Window):
         elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.player_sprite.change_x = 0
 
+    def on_mouse_press(self):
+        """Called whenever mouse button is clicked."""
+
+        #Create a mask bullet
+        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING)
+
+        #Mask bullet speed
+        bullet.change_x = BULLET_SPEED
+
+        #Position mask bullet
+        bullet.center_x = self.player_sprite.center_x
+        bullet.bottom = self.player_sprite.top
+
+        #Add mask bullet to bullet list
+        self.bullet_list.append(bullet)
+
     def on_update(self, delta_time):
         """ Movement and game logic """
 
-        # Call update on all sprites
+        #Call update on bullet sprites
+        self.bullet_list.update()
+        
+        #Call update on all sprites
         self.physics_engine.update()
 
         # -- manage germs propagating --
-        enemy_hit_list = arcade.check_for_collision_with_list(self.player_list, self.enemy_list)
+        for bullet in self.bullet_list:
 
-        for enemy in self.enemy_list:
+            #Check if mask bullet hit germs
+            enemy_hit_list = arcade.check_for_collision_with_list(self.player_list, self.enemy_list)
 
-            while True:
-                #have germs spewing from enemys not killed
-                if enemy in enemy_hit_list:
-                    enemy.remove_from_sprites_lists()
-                #else: 
-                #lauren puts code here that manages virus icons spewing like bullets from the enemys still alive
-                    
+            if len(hit_list) > 2:
+                bullet.remove_from_sprite_lists()
+
+            for enemy in self.enemy_list:
+
+                while True:
+                    #have germs spewing from enemys not killed
+                    if enemy in enemy_hit_list:
+                        enemy.remove_from_sprites_lists()
+                    #else: 
+                    #lauren puts code here that manages virus icons spewing like bullets from the enemys still alive
+
+            #If bullet goes off-screen, remove it
+            if bullet.bottom > SCREEN WIDTH:
+                bullet.remove_from_sprite_lists()
                     
 
         # --- Manage Scrolling ---
